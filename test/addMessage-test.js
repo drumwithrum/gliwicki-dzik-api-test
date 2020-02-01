c45a74ab49cf3804abce499f718fb83664a5ea19
wojewnik10 <wojewodzicn@gmail.com>
2020-02-01 23:43:16 +0100

    Add test for add one message

---
+++ test/addMessage-test.js
@@ -0,0 +1,53 @@
+// localhost:5000/api/null/exercise
+
+const request = require("request");
+const expect = require("chai").expect;
+const baseUrl = "http://localhost:5000/api";
+
+const getAddMessageURL = (userId = null) => `${baseUrl}/${userId}/message/AddMessage`;
+
+const loginData = {
+  username: 'istnieje1112',
+  password: 'testHaslo',
+}
+
+const messageData = {
+    recipientId:49,
+	content:"Od Nikodema do kekw",
+}
+
+const authUrl = baseUrl + '/auth';
+
+describe('Dodawanie wiadomosci', function() {
+  let token = '';
+  let userId = null;
+  
+  before(function(done){
+    request.post(
+      {
+        url: authUrl + '/login',
+        body: loginData,
+        json: true,
+      },
+      function (err, response, body) {
+        token = body.newToken.token;
+        userId = body.userToUse.userId;
+        done();
+      }
+    )
+  })
+
+  it('Zwraca 201 jesli dodano jedna wiadomosc', function(done) {
+    request.post(
+      {
+        url: getAddMessageURL(userId),
+        body:messageData,
+        json: true,
+      },
+      function (err, response, body) {
+         expect(response.statusCode).to.equal(201);
+        done();
+      }
+    ).auth(null, null, true, token)
+  });
+});
