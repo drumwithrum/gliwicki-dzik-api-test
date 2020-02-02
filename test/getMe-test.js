152fa2c550f2522a2612f52ab7a1821bd7953d2b
wojewnik10 <wojewodzicn@gmail.com>
2020-02-02 10:02:37 +0100

    getMe test - change of expects

--- test/getMe-test.js
+++ test/getMe-test.js
@@ -1,17 +1,14 @@
-// localhost:5000/api/null/user/me
+// localhost:5000/api/null/content/getmessage/1
 
 const request = require("request"); 
 const expect = require("chai").expect; 
 const baseUrl = "http://localhost:5000/api"; 
 
-const getMessagesUrl = (userId = null) => `${baseUrl}/${userId}/user/me`;
+const getMeUrl = (userId = null) => ${baseUrl}/${userId}/user/me;
 
 const loginData = {
     username: 'niepisanie',
     password: 'niepisanie',
-    email: "nikos12@gmail.com",
-    gender: "mezczyzna",
-    age: 21,
   }
   const authUrl = baseUrl + '/auth';
 describe('Zalogowanego uzytkownika', function() {
@@ -35,17 +32,14 @@ describe('Zalogowanego uzytkownika', function() {
   it('Zwraca zalogowanego uzytkownika', function(done) {
     request.get(
       {
-        url: getMessagesUrl(userId) , 
+        url: getMeUrl(userId) , 
         json: true, 
       },
       function (err, response, body) { 
-    
-        expect(body.senderId != 0).to.equal(true);
         expect(body.username).to.equal(loginData.username);
-        expect(body.age).to.equal(loginData.age);
-        expect(body.email).to.equal(loginData.email);
-        expect(body.gender).to.equal(loginData.gender);
-        expect(loginData.password.length > 0).to.equal(true);
+        expect(Boolean(body.gender)).to.equal(true);
+        expect(Boolean(body.age)).to.equal(true);
+        expect(Boolean(body.email)).to.equal(true);
         expect(response.statusCode).to.equal(200);
         done();
       }
